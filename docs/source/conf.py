7eaef7063885fd59adbec1dbc06742f41f7713fe
Vladimir Roncevic <elektron.ronca@gmail.com>
2021-12-16 02:29:33 +0100

    [motionbot] added readthedocs

---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+project = u'motionbot'
+copyright = u'2020, Vladimir Roncevic <elektron.ronca@gmail.com>'
+author = u'Vladimir Roncevic <elektron.ronca@gmail.com>'
+version = u'1.0'
+release = u'https://github.com/dof2bot/motionbot/releases/'
+extensions = []
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+language = None
+exclude_patterns = []
+pygments_style = None
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+htmlhelp_basename = 'motionbotdoc'
+latex_elements = {}
+latex_documents = [(
+    master_doc, 'motionbot.tex', u'motionbot Documentation',
+    u'Vladimir Roncevic \\textless{}elektron.ronca@gmail.com\\textgreater{}',
+    'manual'
+)]
+man_pages = [(
+    master_doc, 'motionbot', u'motionbot Documentation', [author], 1
+)]
+texinfo_documents = [(
+    master_doc, 'motionbot', u'motionbot Documentation', author, 'motionbot',
+    'One line description of project.', 'Miscellaneous'
+)]
+epub_title = project
+epub_exclude_files = ['search.html']
